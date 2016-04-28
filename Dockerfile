4bab0a1977113e2c314bc1da9bb3d19fa902d33c
YehaiChen <chenyehai737@126.com>
2016-04-28 20:40:23 +0800

    pip pack

--- Dockerfile
+++ Dockerfile
@@ -17,10 +17,11 @@ RUN pip install flask
 # copy the update and run app shell
 RUN mkdir /myweb
 RUN git -C /myweb clone "https://github.com/chenyehai/GraduateDesigned.git"
+git -C /myweb/GraduateDesigned checkout develop
 RUN pip install -r /myweb/GraduateDesigned/pack_requirements
 COPY update_and_run_app.sh /myweb/
 
-CMD ["sh", "/myweb/update_and_run_app.sh"]
+CMD ["echo","Welcome to use Docker"]
 
 EXPOSE 80:80
 EXPOSE 8080:8080
--- update_and_run_app.sh
+++ update_and_run_app.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env sh
 git -C /myweb/GraduateDesigned checkout develop
 git -C /myweb/GraduateDesigned pull
-mkdir /myweb/GraduateDesigned/database
-python /myweb/GraduateDesigned/manage.py shell < "/myweb/GraduateDesigned/db_init.py"
+
+if [ ! -d "/myweb/GraduateDesigned/database" ]; then
+  mkdir /myweb/GraduateDesigned/database
+  python /myweb/GraduateDesigned/manage.py shell < "/myweb/GraduateDesigned/db_init.py"
+fi
 sh /myweb/GraduateDesigned/start_app.sh &
